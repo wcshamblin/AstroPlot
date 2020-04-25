2dfd6948a2163cfd1e612f6993161d36064bf79a
wcshamblin <will@shamblin.org>
2020-04-25 09:41:15 -0400

    Added black background option

--- AstroPlot.py
+++ AstroPlot.py
@@ -7,11 +7,12 @@ ps = argparse.ArgumentParser(description='Parses and plots data from JPL\'s HORI
 ps.add_argument("path", type=str, nargs="+", help='Path to csv or folder of csv\'s to be plotted')
 ps.add_argument("-f", "--fix", action="store_true", help="Fix aspect ratio/scale of graph")
 ps.add_argument("-s", "--speed", action="store_true", help="Graph speed with marker color")
+ps.add_argument("-b", "--black", action="store_true", help="Use a black background color")
 
-# mirror x/y option
 # date range / animate
 # stats page thing
 # titles?
+#convert units
 
 cmin=0
 cmax=0
@@ -21,6 +22,8 @@ if args.fix:
 	layout = go.Layout(scene=dict(aspectmode='data'))
 else:
 	layout=go.Layout()
+
+
 fig=go.Figure(layout=layout)
 cent={'X':0,'Y':0,'Z':0}
 cent=pd.DataFrame(np.array([[0,0,0]]),columns=['X', 'Y', 'Z'])
@@ -53,4 +56,24 @@ except FileNotFoundError as error:
 if args.speed:
 	fig.update_traces(marker=dict(cmin=cmin, cmax=cmax))
 fig.update_traces(marker=dict(size=1))
+if args.black:
+	fig.update_layout(scene = dict(
+	                    xaxis = dict(
+	                         backgroundcolor="black",
+	                         gridcolor="gray",
+	                         showbackground=True,
+	                         zerolinecolor="gray",),
+	                    yaxis = dict(
+	                        backgroundcolor="black",
+	                        gridcolor="gray",
+	                        showbackground=True,
+	                        zerolinecolor="gray"),
+	                    zaxis = dict(
+	                        backgroundcolor="black",
+	                        gridcolor="gray",
+	                        showbackground=True,
+	                        zerolinecolor="gray",),),
+	                    paper_bgcolor="black", legend=dict(font=dict(color="white"))
+	                  )
+fig.update_layout(margin=dict(r=0,l=0,b=0,t=0),legend=dict(x=0,y=1))
 fig.show()
