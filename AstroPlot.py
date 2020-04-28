81f6f751bcbfd6666a8a09bad0efcf02b6e33eff
wcshamblin <will@shamblin.org>
2020-04-28 11:34:46 -0400

    Added on-the-fly conversions

--- AstroPlot.py
+++ AstroPlot.py
@@ -9,10 +9,7 @@ ps.add_argument("path", type=str, nargs="+", help='Path to csv or folder of csv\
 ps.add_argument("-f", "--fix", action="store_true", help="Fix aspect ratio/scale of graph")
 ps.add_argument("-s", "--speed", action="store_true", help="Graph speed with marker color")
 ps.add_argument("-b", "--black", action="store_true", help="Use a black background color")
-
-# stats page thing
-# titles?
-# convert units
+ps.add_argument("-c", "--convert", type=str, help="Convert units")
 
 cmin=0
 cmax=0
@@ -24,7 +21,11 @@ args=ps.parse_args()
 fig=go.Figure()
 
 fig.update_traces(marker=dict(size=1))
-
+if args.convert is not None:
+	args.convert=args.convert.lower()
+	if args.convert not in ["km-s", "au-d", "km-d"]:
+		warnings.warn("Unit not recognized. Use one of the following: KM-S, AU-D, KM-D. Not attempting to convert.")
+		args.convert=None
 if args.fix:
 	fig.update_layout(scene=dict(aspectmode='data'))
 
@@ -79,13 +80,51 @@ for csv in args.path:
 	orbit.rename(columns=lambda i: i.strip(), inplace=True)
 	orbit.dropna(inplace=True)
 
+
 	for line in inf:
 		if "Target body name:" in line:
 			title=(line.split("Target body name:")[1].split("{")[0]).strip()
 		if "Center geodetic :" in line:
 			center=[float(i) for i in ((line.split("Center geodetic :")[1].split("{")[0]).strip()).split(",")]
 		if "Output units    :" in line:
-			units=(line.split("Output units    :")[1].split("{")[0]).strip()
+			units=(line.split("Output units    :")[1].split("{")[0]).strip().lower()
+
+	if args.convert is not None and units == "":
+		warnings.warn("No units declaration in "+csv+", not attempting to convert")
+
+	else:
+		if args.convert is not None:
+			if args.convert!=units:
+				print(csv+" has "+units.upper()+", converting to "+args.convert.upper())
+				if args.convert[:2]!=units[:2]:
+					if args.convert[:2]=="km":
+						orbit['X']=orbit['X']*149597870.7
+						orbit['Y']=orbit['Y']*149597870.7
+						orbit['Z']=orbit['Z']*149597870.7
+					else:
+						orbit['X']=orbit['X']/149597870.7
+						orbit['Y']=orbit['Y']/149597870.7
+						orbit['Z']=orbit['Z']/149597870.7
+				if args.speed:
+					if set(['VX', 'VY', 'VZ']).issubset(orbit.columns):
+						if args.convert[:2]=="km":
+							orbit['VX']=orbit['VX']/149597870.7
+							orbit['VY']=orbit['VY']/149597870.7
+							orbit['VZ']=orbit['VZ']/149597870.7
+						else:
+							orbit['VX']=orbit['VX']*149597870.7
+							orbit['VY']=orbit['VY']*149597870.7
+							orbit['VZ']=orbit['VZ']*149597870.7
+						if args.convert[3]=="s":
+							orbit['VX']=orbit['VX']*86400
+							orbit['VY']=orbit['VY']*86400
+							orbit['VZ']=orbit['VZ']*86400
+						else:
+							orbit['VX']=orbit['VX']/86400
+							orbit['VY']=orbit['VY']/86400
+							orbit['VZ']=orbit['VZ']/86400
+					else:
+						warnings.warn("Velocity component not present in "+csv+", not able to convert")
 
 	# orbit['Calendar Date (TDB)']=orbit['Calendar Date (TDB)'].str.replace('A.D. ', '')
 	# orbit['Calendar Date (TDB)']=pd.to_datetime(orbit['Calendar Date (TDB)'])
@@ -107,7 +146,7 @@ for csv in args.path:
 			fig.add_trace(go.Scatter3d(x=orbit['X'], y=orbit['Y'], z=orbit['Z'], mode='markers', marker=dict(size=1, color=orbit['TotalSpeed'], colorscale='bluered'), name=title))
 		
 	else:
-		fig.add_trace(go.Scatter3d(x=orbit['X'], y=orbit['Y'], z=orbit['Z'], mode='markers', marker=dict(size=1), name=csv))
+		fig.add_trace(go.Scatter3d(x=orbit['X'], y=orbit['Y'], z=orbit['Z'], mode='markers', marker=dict(size=1), name=title))
 if args.speed:
 	fig.update_traces(marker=dict(cmin=cmin, cmax=cmax))
 fig.show()
